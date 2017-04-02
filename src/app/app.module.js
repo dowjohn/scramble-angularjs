585c4f16046c08e06f6893d3039729b3bf14b15c
john <jldjld503@gmail.com>
2017-04-02 09:36:19 -0700

    An angular skeleton that actually runs

---
+++ src/app/app.module.js
@@ -0,0 +1,14 @@
+import angular from 'angular'
+
+// import { clkButton } from 'components/button'
+// import { clkLabel } from 'components/label'
+//
+// import { clkApp } from './app.component'
+
+export default
+  angular
+    .module('shitty.app', [])
+    // .component('clkApp', clkApp)
+    // .component('clkButton', clkButton)
+    // .component('clkLabel', clkLabel)
+    .name
